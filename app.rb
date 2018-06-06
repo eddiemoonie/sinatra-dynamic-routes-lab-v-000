8bde660cf087a08c927083972e644504bd593cfa
Eddie Moon <eddiemoonie@gmail.com>
2018-06-06 17:11:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,4 +11,12 @@ class App < Sinatra::Base
     "#{@number * @number}"
   end
 
+  get "/say/:number/:phrase" do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    @number.times do
+      "#{@phrase}"
+    end
+  end
+
 end
